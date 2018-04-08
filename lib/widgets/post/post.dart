2d646d1c2715df7a045ffa121cf47cf2a988fbfc
Harrison Turton <harrisonturton@gmail.com>
2018-04-09 00:41:29 +1000

    Shrunk top padding on Post

--- lib/widgets/post/post.dart
+++ lib/widgets/post/post.dart
@@ -18,8 +18,8 @@ class Post extends StatelessWidget {
 			padding: const EdgeInsets.only(
 				left: 20.0,
 				right: 20.0,
-				top: 25.0,
-				bottom: 15.0
+				top: 10.0,
+				bottom: 25.0
 			),
 			child: new Column(
 				mainAxisSize: MainAxisSize.min,
