8b4214a3b1451d1340cb7851c3b68346940db807
Harrison Turton <harrisonturton@gmail.com>
2018-05-07 21:18:03 +1000

    Updated API to get cognito ID from request context

--- chitchat-api/src/functions/list_chatrooms.js
+++ chitchat-api/src/functions/list_chatrooms.js
@@ -2,11 +2,16 @@ import * as dynamoDbLib from '../lib/database';
 import { success, failure  } from '../lib/response';
 
 export async function main(event, context, callback) {
+	// AWS prepends the location, e.g. 'us-east-1:cognito_identity_id',
+	// so remove it before querying.
+	var cognitoId = event.requestContext.identity.cognitoIdentityId;
+	cognitoId = cognitoId.split(':').pop();
+
 	const params = {
 		TableName: 'chatroom',
 		KeyConditionExpression: 'user_id = :user_id',
 		ExpressionAttributeValues: {
-			':user_id': event.requestContext.identity.cognitoIdentityId,
+			':user_id': cognitoId,
 		}
 	};
 
